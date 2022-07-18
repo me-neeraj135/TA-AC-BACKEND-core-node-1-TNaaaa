2435e019fead2e68922288f14f4f96ff0ada7bb8
me-neeraj135 <me.neeraj1455@gmail.com>
2022-07-18 12:32:30 +0530

    request and response object block-4 completed

---
+++ block-BNaaap/index.js
@@ -0,0 +1,16 @@
+let http = require(`http`);
+
+let server = http.createServer(handleRequest);
+
+function handleRequest(req, res) {
+//   req.POST();
+  //   res.statusCode = 201;
+  //   res.setHeader(`content-type`, `Text/html`);
+
+  res.writeHead(200, { ContentType: `Text/html` });
+  res.write(`<h1>welcome to hello world</h1>`);
+  res.end();
+}
+server.listen(4444, () => {
+  console.log(`server listening on port: 4444`);
+});
